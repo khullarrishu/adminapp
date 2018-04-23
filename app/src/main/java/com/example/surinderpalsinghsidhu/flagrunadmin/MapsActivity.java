7b72006e4350b3d7c630d0c9a3ad763baaf013d0
surinderpalsinghsidhu <surindersidhu16@gmail.com>
2018-04-23 18:56:58 -0400

    working project

--- app/src/main/java/com/example/surinderpalsinghsidhu/flagrunadmin/MapsActivity.java
+++ app/src/main/java/com/example/surinderpalsinghsidhu/flagrunadmin/MapsActivity.java
@@ -17,6 +17,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
@@ -34,104 +37,76 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
 
-        root.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                // mMap.clear();
-                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
 
 
 
-                }
-            }
-            @Override
-            public void onCancelled(DatabaseError firebaseError) {
-                /*
-                 * You may print the error message.
-                 **/
-            }
-        });
 
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
 
 
-        root.child("Team A").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-
-                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
-                    Locations location = postSnapshot.getValue(Locations.class);
-                    Log.d("longitude", String.valueOf(location.longitude));
-                    Log.d("latitude", String.valueOf(location.latitude));
-                    Log.d("playerName", String.valueOf(location.Name));
-
-
-
-              LatLng sydney = new LatLng(location.latitude, location.longitude);
-//                    mMap.addMarker(new MarkerOptions().position(sydney).title(location.Name));
-
-
-                    MarkerOptions a = new MarkerOptions().position(new LatLng(location.latitude,location.longitude));
-                     mMap.addMarker(a).setPosition(sydney);
-
-                }
-            }
-            @Override
-            public void onCancelled(DatabaseError firebaseError) {
-                /*
-                 * You may print the error message.
-                 **/
-            }
-        });
-
-
-
-
-
-        root.child("Team B").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-
-
-                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
-                    Locations location = postSnapshot.getValue(Locations.class);
-                    Log.d("longitude", String.valueOf(location.longitude));
-                    Log.d("latitude", String.valueOf(location.latitude));
-                    Log.d("playerName", String.valueOf(location.Name));
-
-
-                    LatLng sydney = new LatLng(location.latitude, location.longitude);
-                    mMap.addMarker(new MarkerOptions().position(sydney).title(location.Name));
-
-
-                }
-            }
-            @Override
-            public void onCancelled(DatabaseError firebaseError) {
-                /*
-                 * You may print the error message.
-                 **/
-            }
-        });
+        getData();
     }
 
 
+public void getData(){
 
+
+
+    root.addValueEventListener(new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot snapshot) {
+            mMap.clear();
+
+            for (DataSnapshot postSnapshot : snapshot.child("Team A").getChildren()) {
+
+
+                Locations location = postSnapshot.getValue(Locations.class);
+
+                Log.d("test longitude", String.valueOf(location.longitude));
+                Log.d("test latitude", String.valueOf(location.latitude));
+                Log.d("test playerName", String.valueOf(location.Name));
+
+
+                LatLng sydney = new LatLng(location.latitude, location.longitude);
+                mMap.addMarker(new MarkerOptions().position(sydney).title(location.Name));
+
+
+            }
+
+
+            for (DataSnapshot postSnapshot : snapshot.child("Team B").getChildren()) {
+
+
+                Locations location = postSnapshot.getValue(Locations.class);
+
+                Log.d("test longitude", String.valueOf(location.longitude));
+                Log.d("test latitude", String.valueOf(location.latitude));
+                Log.d("test playerName", String.valueOf(location.Name));
+
+
+                LatLng sydney = new LatLng(location.latitude, location.longitude);
+                mMap.addMarker(new MarkerOptions().position(sydney).title(location.Name));
+
+
+            }
+
+
+        }
+        @Override
+        public void onCancelled(DatabaseError firebaseError) {
+            /*
+             * You may print the error message.
+             **/
+        }
+    });
+}
 
 
 
